dc1c7bc010057ce9401e62fb58d95d6c78453b58
Khalil Collins <Collinskhalil@hotmail.com>
2023-08-28 15:02:58 -0400

    update on function  lesson

--- functions/functions.ts
+++ functions/functions.ts
@@ -16,3 +16,29 @@ const multiple = function (a: number, b: number): number {
 const logger = (message: string): void => {
 	console.log(message);
 };
+const throwError = (message: string): void => {
+	if (!message) {
+		throw new Error(message);
+	}
+};
+
+const todaysWeather = {
+	date: new Date(),
+	weather: "sunny",
+};
+
+// functionName = ({destructure}): {expected paramaters}: expected return value =>
+
+const logWeather = ({
+	date,
+	weather,
+}: {
+	date: Date;
+	weather: string;
+}): void => {
+	console.log(date);
+	console.log(weather);
+};
+// ES2015
+
+logWeather(todaysWeather);
