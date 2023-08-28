6389f248859ce2c529d3aac75e60ced345f858f3
Khalil Collins <Collinskhalil@hotmail.com>
2023-08-28 15:02:46 -0400

    functions lesson

---
+++ functions/functions.ts
@@ -0,0 +1,18 @@
+const add = (a: number, b: number): number => {
+	return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+	return a - b;
+};
+
+function divide(a: number, b: number): number {
+	return a / b;
+}
+const multiple = function (a: number, b: number): number {
+	return a * b;
+};
+// funcName = (expected parameters): expected return type =>
+const logger = (message: string): void => {
+	console.log(message);
+};
