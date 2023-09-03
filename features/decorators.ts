489ede85c71d4d535179a644c5762e20ea4f5f74
Khalil Collins <Collinskhalil@hotmail.com>
2023-09-03 16:05:01 -0400

    Express lesson 1 end

---
+++ features/decorators.ts
@@ -0,0 +1,46 @@
+// @Decorators can not access variables
+@classDecorator
+class Boat {
+	@testDecorator
+	color: string = "red";
+
+	get formattedColor(): string {
+		return `This boats color is ${this.color}`;
+	}
+	//function decorator
+	@logError("oops the boat was sunk")
+	// parameter decorators
+	pilot(@parameterDecorator speed: string): void {
+		if (speed === "swish") {
+			console.log("swish");
+		} else {
+			console.log("nothing");
+		}
+	}
+}
+
+function classDecorator(constructor: typeof Boat) {
+	console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+	console.log(key, index);
+}
+
+function testDecorator(target: any, key: string) {
+	console.log(target);
+	console.log(key);
+}
+function logError(errorMessage: string) {
+	return function (target: any, key: string, desc: PropertyDescriptor): void {
+		const method = desc.value;
+
+		desc.value = function () {
+			try {
+				method();
+			} catch (error) {
+				console.log(errorMessage);
+			}
+		};
+	};
+}
