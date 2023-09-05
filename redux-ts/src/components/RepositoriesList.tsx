d674dcdedba2c05b8e60903494c6c39059b33c96
Khalil Collins <Collinskhalil@hotmail.com>
2023-09-05 12:15:55 -0400

    React-redux typescript lesson

---
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+  const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    searchRepositories(term);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+        <button>search</button>
+      </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>{loading}</h3>}
+      {!error && !loading && <h3>{data}</h3>}
+    </div>
+  );
+};
+
+export default RepositoriesList;
